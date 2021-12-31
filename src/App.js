25edbd5d591398c2f8d184a663a9ea8ae5c75ac9
robelabiye <rblabiye5@gmail.com>
2021-12-31 08:25:10 +0300

    day4

--- src/App.js
+++ src/App.js
@@ -2,37 +2,92 @@ import ReactFCCtest from 'react-fcctest';
 import React from 'react';
 import './App.css';
 
-const pads = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]
 
+const audioClips = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
 class App extends React.Component {
+
   render (){
     return (
       <div id="drum-machine">
-        <div id="display" className="display">
-        {pads.map((keys, idx)=>(
-            <Box  text={keys} key={idx} />
-        ))}
+        <div className="header">
+        <h1>Drum Machine</h1>
         </div>
-          <Box />
+        <ReactFCCtest />
+        <div id="display" className="display">
+        {audioClips.map((keys, inx) => {
+            return (
+                <Box text={keys.keyTrigger} key={keys.url}/>
+            );
+           } 
+        )}
+        </div>
       </div>
-     );
-   }
+    )
+  };
 }
 
 class Box extends React.Component {
     render (){
         return (
-            <div className="box">
-            {this.props.text}
-            </div>
+                <button className="box drum-pad" id={this.props.text}> {this.props.text} 
+                </button>
         )
     };
     
 }
-
-      //<ReactFCCtest />
-     //<div id="display"></div>
-     //{pads.map(i => <a className="drum-pads" id={i} href='"#"+{i}'>{i}</a>)}
-     //</div>
-
 export default App;
